af2555c66b7fb01df4b3a15ec067fa9da97f3ebc
JoshMiller6850 <joshmiller6850@gmail.com>
2018-10-20 14:58:19 -0500

    Update THR33.java

---
+++ THR33.java
@@ -0,0 +1,17 @@
+/*
+CSCE 111
+Team Alpha
+Josh Miller
+Ryan Brandt
+John Woodruff
+Andrew Su
+
+Project 1 - Thr33
+Made on Windows
+10-26-18
+*/
+class THR33{
+  public static void main(String[] args){
+String Cards[]= {Q-D};
+  }//End Main Method
+}//End class THR33
